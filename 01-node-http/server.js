0579b9cc3cabcccc3ce4b9df8926d0773fca902d
viitorfernandess <vitorazevedofer007@gmail.com>
2025-06-25 18:34:37 -0300

    Cria o servidor HTTP e define a função apra lidar com as requisições

---
+++ 01-node-http/server.js
@@ -0,0 +1,20 @@
+// Importa o módulo HTTP do Node.js
+const http = require('node:http')
+
+// Cria o servidor HTTP e define a função para lidar com as requisições
+const server = http.createServer((request, response) => {
+    // Imprime no console o objeto da requisição recebida
+    console.log(request)
+
+    // Define o status da resposta HTTP como 200 (OK)
+    response.writeHead(200)
+
+    // Envia o corpo da resposta com uma mensagem simples
+    response.write('Servidor HTTP em Node.js funcionando!')
+
+    // Finaliza a resposta (obrigatório para indicar que a resposta está completa)
+    response.end()
+})
+
+
+server.listen()
