44a6dfc243389b22e8e8d33e9fe8a8cc6be8d7e2
viitorfernandess <vitorazevedofer007@gmail.com>
2025-06-27 16:24:09 -0300

    melhorias na estrutura do express

--- 02-express/src/server.js
+++ 02-express/src/server.js
@@ -1,3 +1,17 @@
 const express = require('express')
 
 const server = express()
+
+server.get('/', (request, response) => {
+    response.send('Servidor Express funcionando!\nVocê está na página inicial.')
+})
+
+server.get('/artigos', (req, res) => {
+    res.send('Você está na página de artigos!')
+})
+
+const PORT = 3000
+
+server.listen(PORT, () => {
+    console.log(`Servidor Express iniciado em http://localhost:${PORT}/`)
+})
