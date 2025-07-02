773e3c4980661a7f0f355cbe683730b949489599
viitorfernandess <vitorazevedofer007@gmail.com>
2025-07-02 15:57:18 -0300

    Renderizando html dinâmico com EJS

--- 04-html-dinamico-com-ejs/src/index.js
+++ 04-html-dinamico-com-ejs/src/index.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const path = require('node:path')
 
 const app = express()
 
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    const title = 'Homepage'
+    const message = 'Mensagem dinâmica inserida pelo EJS'
+    res.render('index', { title, message})
 })
 
 const PORT = 3000
