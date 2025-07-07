78174c40a3caad32abd37e056b9cb7d8b17d463c
viitorfernandess <vitorazevedofer007@gmail.com>
2025-07-07 14:45:12 -0300

    criação de formulário de registro

--- 04-html-dinamico-com-ejs/src/index.js
+++ 04-html-dinamico-com-ejs/src/index.js
@@ -2,14 +2,28 @@ const express = require('express')
 const path = require('node:path')
 
 const app = express()
-
+//Configuração do EJS
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
+
+//Configuração do body
+app.use(express.urlencoded({extended: true}))
 
 app.get('/', (req, res) => {
     const title = 'Homepage'
     const message = 'Mensagem dinâmica inserida pelo EJS'
     res.render('index', { title, message})
+})
+
+app.get('/formulario', (req, res) => {
+    res.render('form')
+})
+
+app.post('/register', (req, res) => {
+    const username = req.body.username
+    console.log(req.body)
+
+    res.send()
 })
 
 const PORT = 3000
