98248a42d2d2f2e4e1057f7ed30b2940cbd50c90
viitorfernandess <vitorazevedofer007@gmail.com>
2025-08-04 15:33:22 -0300

    Arquitetura MVC. Criação do servidor

---
+++ 06-projeto-mvc/src/models/postModel.js
@@ -0,0 +1,38 @@
+let posts = []
+
+// Post { id, title, content, creatAt, updateAt}
+const postModel = {
+    getAllPosts() {
+        return posts
+    },
+
+    getPostsById(id) {
+        return posts.find(post => post.id == id)
+    },
+
+    createPost(title, content) {
+        const post = {
+            id: Date.now().toString(),
+            title: title,
+            content: content,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }
+        return post
+    },
+
+    savePost(post) {
+        posts.push(post)
+    },
+
+    updatePost(id, updatedPost) {
+const index = posts.findIndex(post => post.id === id)
+posts[index] = { ...posts[index], ...updatedPost, updatedAt: new Date()}
+    },
+
+    deletePost(id) {
+posts = posts.filter(post.id !== id)
+    }
+}
+
+module.exports = postModel
