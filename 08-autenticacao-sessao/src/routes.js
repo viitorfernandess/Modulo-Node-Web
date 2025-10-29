1ec9c66493eed42b18667d7261e5f167f3ef7dab
viitorfernandess <vitorazevedofer007@gmail.com>
2025-10-29 14:58:03 -0300

    Autenticação simples

--- 08-autenticacao-sessao/src/routes.js
+++ 08-autenticacao-sessao/src/routes.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const dashboardController = require('./controllers/dashboard-controller')
 
 const router = express.Router()
+
+router.get('/dashboard', dashboardController.dashboard)
 
 module.exports = router
