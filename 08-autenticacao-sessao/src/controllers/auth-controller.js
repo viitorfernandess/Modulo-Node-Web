0cfb872ae8daa7dd54ac16733744bd4329e7283c
viitorfernandess <vitorazevedofer007@gmail.com>
2025-10-30 17:53:38 -0300

    Autenticação simples

--- 08-autenticacao-sessao/src/controllers/auth-controller.js
+++ 08-autenticacao-sessao/src/controllers/auth-controller.js
@@ -1,4 +1,30 @@
+let users = [
+    { username: 'Vitor', password: '123456' },
+    { username: 'marcelo', password: '654321' }
+]
+
+
 module.exports = {
     // GET /
-    
+    index: (req, res) => {
+        res.render('index')
+    },
+
+    // POST /auth/register
+    register: (req, res) => {
+        const { username, password } = req.body
+
+        const userAlreadyExists = users.find(user => user.username === username)
+        if (userAlreadyExists) {
+            return res.status(400).redirect('/')
+        }
+
+        const newUser = { username, password }
+        user.push(newUser)
+        res.redirect('/dashboard')
+
+    }
+    // POST /auth/login
+
+    // GET /auth/logout
 }
--- 08-autenticacao-sessao/src/routes.js
+++ 08-autenticacao-sessao/src/routes.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const dashboardController = require('./controllers/dashboard-controller')
+const authController = require('./controllers/auth-controller')
 
 const router = express.Router()
 
+router.get('/', authController.index)
+router.post('/auth/register', authController.register)
 router.get('/dashboard', dashboardController.dashboard)
 
 module.exports = router
