3231428bda4096c5271565463bce68bf647c9302
viitorfernandess <vitorazevedofer007@gmail.com>
2025-11-04 15:15:29 -0300

    feat(auth): adiciona a rota POST /register (cria usuário)

--- 09-autenticacao-jwt/src/routes/auth.js
+++ 09-autenticacao-jwt/src/routes/auth.js
@@ -4,5 +4,14 @@ const express = require('express')
 // Cria um "roteador" (Router) — uma mini-instância do Express usada para organizar rotas
 const authRouter = express.Router()
 
+authRouter.post('/register', (req, res) => {
+    const { username, password } = req.body
+
+    const user = { username, password }
+    users.push(user)
+
+    res.status(201).json(user)
+})
+
 // Exporta o roteador para que ele possa ser importado e usado em outro arquivo (ex: server.js)
 module.exports = authRouter
