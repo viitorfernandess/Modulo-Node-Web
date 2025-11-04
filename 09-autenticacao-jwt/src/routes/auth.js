0097ea6c6ff522de0ca9b74850eff2d72f327c1b
viitorfernandess <vitorazevedofer007@gmail.com>
2025-11-04 15:45:09 -0300

    feat: adiciona rota de login de usuário

--- 09-autenticacao-jwt/src/routes/auth.js
+++ 09-autenticacao-jwt/src/routes/auth.js
@@ -15,5 +15,16 @@ authRouter.post('/register', (req, res) => {
     res.status(201).json(user)
 })
 
+authRouter.post('/login', (req, res) => {
+    const { username, password } = req.body
+
+    const user = users.find(user => user.username === username)
+    if (!user || user.password !== password) {
+        return res.status(401).json({ message: 'Credenciais inválidas' })
+    }
+
+    res.json({})
+})
+
 // Exporta o roteador para que ele possa ser importado e usado em outro arquivo (ex: server.js)
 module.exports = authRouter
