cfe922f092041b6e8055b9511d13251c9516b9a9
viitorfernandess <vitorazevedofer007@gmail.com>
2025-11-04 14:42:51 -0300

    feat(server): adiciona routers auth e protected ao servidor

--- 09-autenticacao-jwt/src/server.js
+++ 09-autenticacao-jwt/src/server.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const authRouter = require('./routes/auth')
+const protectedRouter = require('./routes/protected')
 
 const app = express()
 
 app.use(express.json())
 
+app.use(authRouter)
+app.use(protectedRouter)
 
 app.listen(3000, () => console.log('Servidor inicado!') )
