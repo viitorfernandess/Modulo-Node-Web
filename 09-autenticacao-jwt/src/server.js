1c24b3a6a494a76130d54c1391d4382d4279ee59
viitorfernandess <vitorazevedofer007@gmail.com>
2025-11-04 14:46:46 -0300

    refactor(server): define caminhos base /auth e /protected

--- 09-autenticacao-jwt/src/server.js
+++ 09-autenticacao-jwt/src/server.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(express.json())
 
-app.use(authRouter)
-app.use(protectedRouter)
+app.use('/auth', authRouter)
+app.use('/protected', protectedRouter)
 
 app.listen(3000, () => console.log('Servidor inicado!') )
