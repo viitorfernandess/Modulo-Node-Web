933341f854eaac8d0780ad9257f350a630dd5559
viitorfernandess <vitorazevedofer007@gmail.com>
2025-11-06 14:51:40 -0300

    feat: adiciona rota /protected/dashboard

--- 09-autenticacao-jwt/src/middleware/auth-middleware.js
+++ 09-autenticacao-jwt/src/middleware/auth-middleware.js
@@ -2,4 +2,10 @@ const jwt = require('jsonwebtoken')
 
 const authMiddleware = (req, res, next) => {
     const authHeader = req.headers.authorization
+
+    console.log(authHeader)
+
+    next()
 }
+
+module.exports = authMiddleware
--- 09-autenticacao-jwt/src/routes/protected.js
+++ 09-autenticacao-jwt/src/routes/protected.js
@@ -1,8 +1,13 @@
 // Importa o módulo Express para poder usar suas funções
 const express = require('express')
+const authMiddleware = require('../middleware/auth-middleware')
 
 // Cria um "roteador" (Router) — uma mini-instância do Express usada para organizar rotas
 const protectedRouter = express.Router()
+
+protectedRouter.get('/dashboard',authMiddleware, (req, res) => {
+    res.json({ message: 'Esta é a área protegida'})
+})
 
 // Exporta o roteador para que ele possa ser importado e usado em outro arquivo (ex: server.js)
 module.exports = protectedRouter
